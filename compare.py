dcf7ccc09f590e2f48e398cd56fa953d19dbac3c
nmuxiang <nmuxiang@163.com>
2016-07-06 11:15:42 +0800

    继续写

--- compare.py
+++ compare.py
@@ -1,6 +1,7 @@
 import tkinter.filedialog
 import xlrd
 import sys
+import json
 
 def tupletodict(a):
     b={}
@@ -19,18 +20,29 @@ def sheetstodict(a):
         for s in wb.sheets():
            shts[i]=s.name
            i=i+1
+           c=readcelltodict(s)
         b[key]=shts
+        
     return b
 
-def readcelltodict(a):
-    for row in range(a.nrows):
+##def readcelltodict(a,d):
+##    b={}
+##    for key,value in a.items():
+##        c=d[value]
+##        print(c)
+##        for row in range(a.nrows):
+##            for col in range(a.ncols):
+##                b[xlrd.cellname(row,col)]=a.cell(row,col).value
+##    return b
         
 
-try:
+try :
     readFile=tkinter.filedialog.askopenfilenames()
+    d=json.load(open('/setting.json','r'))
+    print(d)
     b=tupletodict(readFile)
     c=sheetstodict(b)
-    print(c)
+    #d=readcelltodict(c)
 except FileNotFoundError:
     sys.exit()
 
