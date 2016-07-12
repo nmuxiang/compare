6ac71db40cae3b1a3f2306f920ae35ab238e9f47
nmuxiang <nmuxiang@163.com>
2016-07-13 00:50:43 +0800

    reg expression
    reg expression needs to convert location of cell to number in excel

--- compare.py
+++ compare.py
@@ -3,56 +3,59 @@ import xlrd
 import sys
 import json
 
+#读取文件名
 def tupletodict(a):
     b={}
     for item in a:
         b[item]=''
     return b
 
+#读取每个文件中的表名
 def sheetstodict(a):
     b={}
     for key,value in a.items():
         wb=xlrd.open_workbook(key)
         shts={}
         for s in wb.sheets():
-           shts[s.name]=''
+            c=readcelltodict(s)
+            shts[s.name]=c
         b[key]=shts
     return b
 
+#不载入配置文件读取每个表中单元格的值和位置
 def readcelltodict(a):
     b={}
-    for key,value in a.items():
-        for row in range(a.nrows):
-            for col in range(a.ncols):
-                b[xlrd.cellname(row,col)]=a.cell(row,col).value
+    for row in range(a.nrows):
+        for col in range(a.ncols):
+            b[xlrd.cellname(row,col)]=a.cell(row,col).value
     return b
 
+#载入配置文件读取每个表中单元格的值和位置
 def readcelltodict(a,d):
     b={}
-    for key,value in a.items():
-        c=d[value]
-        print(c)
-        for row in range(a.nrows):
-            for col in range(a.ncols):
-                b[xlrd.cellname(row,col)]=a.cell(row,col).value
+    for key,value in d.items():
+        if a.name==d.key:
+            for row in range(a.nrows):
+                for col in range(a.ncols):
+                    b[xlrd.cellname(row,col)]=a.cell(row,col).value
     return b
         
 def readfilenosetting(a='n'):
     readFile=tkinter.filedialog.askopenfilenames()
     b=tupletodict(readFile)
     c=sheetstodict(b)
-    d=readcelltodict(c)
+
     
 def readfilesetting(a='y'):
-    if a=='n':
-        pass
-    elif a=='y':
-        try:
-            d=json.load(open('/setting.json','r'))
-        except IOError:
-            print('open file error')
-        except FileNotFoundError:
-            print('can not find setting.json')
+    try:
+        d=json.load(open('/setting.json','r'))
+    except IOError:
+        print('open file error')
+    except FileNotFoundError:
+        print('can not find setting.json')
+    readFile=tkinter.filedialog.askopenfilenames()
+    b=tupletodict(readFile)
+    c=sheetstodict(b)
 
 #主函数
 def main():
